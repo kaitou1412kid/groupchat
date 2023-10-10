af2c7d6d3be9082fa7825df82812a6d83bbb37f6
kaitou1412kid <kritagyashakya0@gmail.com>
2023-10-10 15:05:01 +0545

    leave group url

--- urls.py
+++ urls.py
@@ -11,5 +11,6 @@ urlpatterns = [
     path('creategroup/', views.create_group, name='create_group'),
     path('joingroup/<int:group_id>/', views.join_group, name='join_group'),
     path('leavegroup/<int:group_id>/', views.leave_group, name='leave_group'),
+    path('members/<int:group_id>/', views.group_members, name='members_group'),
     path('groupchat/<int:group_id>/', views.group_chat, name='group_chat'),
 ]
